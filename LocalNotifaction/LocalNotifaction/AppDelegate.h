995a1dc7e1b66db11af306d89584ea962c474941
alan.zhang <alan.zhang@kakaocorp.com>
2016-03-23 15:26:26 +0800

    初始化项目

---
+++ LocalNotifaction/LocalNotifaction/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocalNotifaction
+//
+//  Created by kakao on 16/3/23.
+//  Copyright © 2016年 shuai zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocalNotifaction/LocalNotifaction/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocalNotifaction
+//
+//  Created by kakao on 16/3/23.
+//  Copyright © 2016年 shuai zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
